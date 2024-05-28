1b9b88f7c6eb322cdc2428e1269bf23aecd93d71
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-28 11:49:06 +0530

    Changes to be committed:  	modified:   .vscode/launch.json 	modified:   problem7.java 	new file:   problem8.java

---
+++ problem8.java
@@ -0,0 +1,23 @@
+public class problem8 {
+    public static int maxProfit(int [] prices)
+    {
+        int buyprice = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i] < buyprice)
+            {
+                buyprice = prices[i];
+            }else{
+                profit = Math.max(profit, prices[i] - buyprice);
+            }
+        }
+        return profit;
+    }
+    public static void main(String args [])
+    {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("Maximum profit is : "+maxProfit(prices));
+    }
+}
