fce92642bcd9cd2662c4a0019098569a4c86a0d6
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-28 11:58:53 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   problem10.java

---
+++ problem10.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class problem10 {
+    public static int findKthLargest(int nums[],int k)
+    {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+    public static void main(String args [])
+    {
+        int nums[] = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));
+    }
+}
