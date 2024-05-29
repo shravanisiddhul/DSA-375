55849e7fa76e57567ec9f6fa4529c5ee94ebb8ac
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-29 21:17:00 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   problem14.java

---
+++ problem14.java
@@ -0,0 +1,31 @@
+public class problem14 {
+    public static int findMin(int[] nums)
+    {
+        int left = 0;
+        int right = nums.length - 1;
+
+        if(nums[left] < nums[right])
+        {
+            return nums[left];
+        }
+
+        while(left < right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[right] < nums[mid])
+            {
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String args [])
+    {
+        int [] nums = {3, 4, 5, 6, 1, 2};
+
+        System.out.println(findMin(nums));
+    }
+}
