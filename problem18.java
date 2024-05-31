da852f8b52713b47dbc1b1498331f7df9c9da86d
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-05-31 21:23:55 +0530

    Changes to be committed: 	new file:   problem18.java

---
+++ problem18.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class problem18 {
+
+    // 	Given Sum Pair
+    public static int countPair(int a[],int sum,int n)
+    {
+        int i = 0;
+        int j = n - 1;
+        int k = 0;
+        int ans = 0;
+
+        while(i < j)
+        {
+            k = a[i] + a[j];
+            if(k == sum)
+            {
+                ans++;
+                i++;
+                j--;
+            }else if(sum > k)
+            {
+                i++;
+            }else{
+                j--;
+            }
+        }
+        if(ans == 0)
+        {
+            return -1;
+        }
+        return ans;
+    }
+
+    public static void main(String args [])
+    {
+        int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        Arrays.sort(a);
+        int sum = 10;
+        int result = countPair(a, sum, a.length);
+
+        if(result == -1)
+        {
+            System.out.println("no pair is found");
+        }else{
+            System.out.println("Number of pairs are : "+result);
+        }
+        
+        
+    }
+}
+
