52e25f9cc9cfe77545b7bcb7b9fa06a24f7380fc
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-06-02 19:25:18 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	new file:   problem20.java

---
+++ problem20.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class problem20 {
+    
+    // Merge Overlapping Intervals
+
+    public static  int[][] overlappedInterval(int[][] Intervals)
+    {
+        if(Intervals == null || Intervals.length == 0)
+        {
+            return new int[0][0];
+        }
+
+        Arrays.sort(Intervals, (a,b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currInterval = Intervals[0];
+        mergedIntervals.add(currInterval);
+
+        for(int[] interval : Intervals)
+        {
+            int currEnd = currInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+
+            if(currEnd >= nextStart)
+            {
+                currInterval[1] = Math.max(currEnd, nextEnd);
+            }else{
+                currInterval = interval;
+                mergedIntervals.add(currInterval);
+            }
+        }
+        return mergedIntervals.toArray(new int [mergedIntervals.size()][]);
+    }
+
+    public static void main(String args [])
+    {
+        int [][] Intervals = {{1,3},{2,4},{6,8},{9,10}};
+        int[][] result = overlappedInterval(Intervals);
+
+        System.out.println(Arrays.deepToString(result));
+    }
+}
