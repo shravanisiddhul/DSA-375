51f7045d1cecab11e331b1e4931235eb49d19062
Shravani Siddhul <shravanisiddhul1473@gmail.com>
2024-06-03 09:43:17 +0530

    Changes to be committed: 	new file:   List.java 	modified:   practice.java

--- practice.java
+++ practice.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class practice
 {
-    public static List<List<Integer>> threeSum(int [] nums)
+    public static List<List<Integer>>threeSum(int [] nums)
     {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
@@ -16,47 +16,47 @@ public class practice
             {
                 continue;
             }
-
             if(nums[i] > 0)
             {
                 break;
             }
 
-            int j = i + 1;
-            int k = n - 1;
+            int j = i+1;
+            int k = n-1;
 
-            while(j < k)
+            while(j<k)
             {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0)
-                {
-                    j++;
-                }else if(sum > 0)
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum > 0)
                 {
                     k--;
+                }else if(sum< 0)
+                {
+                    j++;
                 }else{
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    List<Integer> temp =  Arrays.asList(nums[i],nums[j],nums[k]);
                     ans.add(temp);
+
                     j++;
                     k--;
 
-                    while(j < k && nums[k] == nums[k+1])
-                    {
-                        k--;
-                    }
-                    while(j < k && nums[j] == nums[j-1])
+                    while(j<k && nums[j] == nums[j-1])
                     {
                         j++;
                     }
+                    while(j<k && nums[k] == nums[k+1])
+                    {
+                        k--;
+                    }
                 }
             }
+            
         }
         return ans;
     }
-
     public static void main(String args [])
     {
-        int nums[] = {0,0,0};
+        int nums[] = {-1,0,1,2,-1,-4};
 
         List<List<Integer>> result = threeSum(nums);
         System.out.println(result);
